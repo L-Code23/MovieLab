ecd2040f471a9044317aef4395f3d8dfda30204d
Ramsey Louder <RamseyLouder@gmail.com>
2024-05-13 23:11:12 -0400

    first commit, not complete

---
+++ Movie.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace MovieLab
+{
+//    internal class MovieSelect
+//    {
+//        //properties
+
+//        public string title {  get; set; }  
+//        public string category {  get; set; }
+
+//        //constructors
+
+//        public MovieSelect (string _title, string _category)
+//        {
+//            title = _title;
+//            category = _category;   
+
+//        }
+
+//        ////methods
+//        //public string Categories()
+//        //{
+//        //    foreach()
+//        //}
+    }
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MovieLab;
+using System.Collections.Generic;
+using System.Diagnostics;
+Console.WriteLine("Please select a movie category.");
+string input = Console.ReadLine();
+//CategorySelect = new MovieSelect(input);
+
+List<KeyValuePair<string, string>>Select = new List<KeyValuePair<string,string>>();
+Select.Add(new KeyValuePair<string, string>("The Mighty Ducks", "Sports"));
+Select.Add(new KeyValuePair<string, string>("Rudy", "Sports"));
+Select.Add(new KeyValuePair<string, string>("Remember The Titans", "Sports"));
+Select.Add(new KeyValuePair<string, string>("Above The Rim", "Sports"));
+Select.Add(new KeyValuePair<string, string>("Rocky", "Sports"));
+Select.Add(new KeyValuePair<string, string>("Waynes World", "Comedy"));
+Select.Add(new KeyValuePair<string, string>("The Big Lebowski", "Comedy"));
+Select.Add(new KeyValuePair<string, string>("Coming To America", "Comedy"));
+Select.Add(new KeyValuePair<string, string>("Purple Rain", "Music"));
+Select.Add(new KeyValuePair<string, string>("The God Father", "Drama"));
+
+List<string> MovieCat = Select.Count(t => Select.Contains( input)).ToList();
+
+
+
+
+
+   
